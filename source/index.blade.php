3223d973fb7cd3721181e21869a8124ec1e22a88
Gareth Drew <gareth@cyber-duck.co.uk>
2019-09-27 16:18:04 +0100

    Added Jigsaw and Tailwind

---
+++ source/index.blade.php
@@ -0,0 +1,36 @@
+@extends('_layouts.master')
+
+@section('body')
+<h1>Hello, I'm Gareth, Technical director based in London, UK.</h1>
+
+
+<h2>About me</h2>
+<p>I have over 11 years experience in web development</p>
+<p></p>
+
+<h2>Technologies that I work with</h2>
+<h3>Backend</h3>
+<ul class="bullets-inline pl-0">
+    <li>Laravel</li>
+    <li>PHP</li>
+    <li>MySQL</li>
+    <li>MongoDB</li>
+    <li>Algolia</li>
+    <li>Elasticsearch</li>
+    <li>Redis</li>
+    <li>Memcached</li>
+</ul>
+
+<h3>DevOps</h3>
+<ul class="bullets-inline pl-0">
+    <li>AWS</li>
+    <li>Azure</li>
+    <li>Terraform</li>
+    <li>Ansible</li>
+    <li>Algolia</li>
+    <li>Elasticsearch</li>
+    <li>Redis</li>
+    <li>Memcached</li>
+</ul>
+
+@endsection
